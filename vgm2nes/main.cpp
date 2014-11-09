41102ea21fe96b17678be8b68c1ff66d2989d5ef
osoumen <osoumen@rice-2.local>
2014-11-09 18:38:41 +0900

    Initial Commit

---
+++ vgm2nes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vgm2nes
+//
+//  Created by Takahiko Fujita on 2014/11/09.
+//  Copyright (c) 2014年 osoumen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
